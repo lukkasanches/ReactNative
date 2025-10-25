e2931a518b415c0b5a1b7a5db0875763c17e56b2
lukkasanches <lukadorock2008@gmail.com>
2025-10-25 13:55:05 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import { View } from 'react-native';
+import UseStateHook from './components/UseStateHook';
+import UseReducerHook from './components/UseReducerHook';
+
+export default function App() {
+  return (
+    <>
+      <UseStateHook/>
+    </>
+  );
+}
+
